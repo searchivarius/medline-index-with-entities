e3e4cff3e400c5ae406684fbcd96c4b6ac338c6e
searchivarius <leo@boytsov.info>
2015-08-04 22:30:48 -0400

    Split concept IDs on both '|' and ','

--- src/main/java/edu/cmu/lti/oaqa/bio/index/medline/annotated/uima/MedlineCollectionReader.java
+++ src/main/java/edu/cmu/lti/oaqa/bio/index/medline/annotated/uima/MedlineCollectionReader.java
@@ -16,6 +16,7 @@
 package edu.cmu.lti.oaqa.bio.index.medline.annotated.uima;
 
 import java.util.*;
+import java.util.regex.Pattern;
 import java.io.*;
 
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
@@ -31,9 +32,7 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-
 import edu.cmu.lti.oaqa.annographix.solr.UtilConst;
-
 import edu.cmu.lti.oaqa.bio.index.medline.annotated.types.DocInfo;
 import edu.cmu.lti.oaqa.bio.index.medline.annotated.types.Entity;
 import edu.cmu.lti.oaqa.bio.index.medline.annotated.types.EntityConceptId;
@@ -43,7 +42,6 @@ import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.collection.CollectionReader_ImplBase;
-
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.Progress;
@@ -114,7 +112,7 @@ public class MedlineCollectionReader extends CollectionReader_ImplBase {
   private StanfordCoreNLP           mPipeline = null;
   private static Splitter           mSplitOnTAB = Splitter.on('\t');
   private static Splitter           mSplitOnNL  = Splitter.on('\n');  
-  private static Splitter           mSplitOnPipe  = Splitter.on('|');
+  private static Splitter           mSplitOnPipe  = Splitter.on(Pattern.compile("[,|]"));
   
   private XmlHelper mXmlHelper = new XmlHelper();
 
@@ -382,5 +380,11 @@ public class MedlineCollectionReader extends CollectionReader_ImplBase {
   @Override
   public void close() throws IOException {
     System.out.println("Processed " + mEntryQty + " abstracts");    
+  }
+  
+  public static void main(String args[]) {
+    for (String s : mSplitOnPipe.split("this,is|a simple,example|!")) {
+      System.out.println(s);
+    }
   }
 }
